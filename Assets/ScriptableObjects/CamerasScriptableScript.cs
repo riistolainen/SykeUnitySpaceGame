cdc347753f221feaded5efcf111b92508b1aeccd
Riisto <riistolainen@gmail.com>
2026-06-08 12:14:36 +0300

    I love UI

--- Assets/ScriptableObjects/CamerasScriptableScript.cs
+++ Assets/ScriptableObjects/CamerasScriptableScript.cs
@@ -1,18 +1,18 @@
 using Unity.Cinemachine;
 using Unity.Properties;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class CamerasScriptable : MonoBehaviour
+[CreateAssetMenu(fileName = "CamerasScriptable", menuName = "Scriptable Objects/CamerasScriptable")]
+public class CamerasScriptableScript : ScriptableObject
 {
     [CreateProperty] public CinemachineCamera CameraFollow;
     [CreateProperty] public CinemachineCamera CameraOverhead;
     [CreateProperty] public CinemachineCamera CameraFreefly;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/AdminUI.cs
+++ Assets/Scripts/AdminUI.cs
@@ -1,12 +1,50 @@
+//ATTACHED TO UIDOCUMENT -obj
+
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AdminUIScript : MonoBehaviour
 {
+
+    CamerasScriptable camerasScriptable;
+
+    private void OnTestToggleChanged(ChangeEvent<bool> evt)
+    {
+        // Handling code
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        camerasScriptable = transform.parent.parent.Find("Cameras").GetComponent<CamerasScriptable>();
         
+        UIDocument uiDocument = GetComponent<UIDocument>();
+        VisualTreeAsset vTA = uiDocument.GetComponent<VisualTreeAsset>();
+        RadioButtonGroup rbg = vTA.GetComponent<RadioButtonGroup>();
+
+        //rbg.ElementAt(0).Bind(camerasScriptable.CameraFollow);
+        //rbg.ElementAt(0).SetBinding(BindingId );
+        rbg.ElementAt(0).dataSource = camerasScriptable.CameraFollow;
+        rbg.ElementAt(1).dataSource = camerasScriptable.CameraOverhead;
+        rbg.ElementAt(2).dataSource = camerasScriptable.CameraFreefly;
+
+        rbg.ElementAt(0).RegisterCallback<ChangeEvent<Toggle>> (Myevt);
+        rbg.RegisterValueChangedCallback(evt => { });
     }
+
+    //TODO: React to UI change through methods and update which camera is active?
+    void Myevt(ChangeEvent<Toggle> evt)
+    { 
+        //TODO this might be complete wrong setup
+    
+    }
+    private void OnBoolChangedEvent(ChangeEvent<bool> evt)
+    {
+        Debug.Log($"Toggle changed. Old value: {evt.previousValue}, new value: {evt.newValue}");
+    }
+
 
     // Update is called once per frame
     void Update()
